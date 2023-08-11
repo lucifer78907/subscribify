a9814e2532015cb175a40db85314c8ae02eff9d7
Rudra Pratap Singh <chaudhary66667@gmail.com>
2023-08-11 08:19:13 +0530

    +Finalized Billing/Payment

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const stripe = require("stripe")(
   "sk_test_51NdcsKSBL2yANXaz2BeOxDkbaY5ha9YPEtNoNaX2CoeNp4wy4uKYriBvqxOarb8LNtOHzTWQkuJGOSWDq8jMvPvJ00qKr7elcF"
 );
+const PlanData = require("../models/PlanData");
 
 router.get("/config", (req, res) => {
   res.send({
@@ -10,16 +11,35 @@ router.get("/config", (req, res) => {
   });
 });
 
-router.post("/create-payment", async (req, res) => {
-  const paymentIntent = await stripe.paymentIntents.create({
-    currency: "inr",
-    amount: 100,
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+router.post("/create-payment", async (req, res, next) => {
+  const planName = req.body.selectedPlan;
+  const duration = req.body.duration;
 
-  res.send({ clientSecret: paymentIntent.client_secret });
+  try {
+    const plan = await PlanData.findOne({ "Plan Name": planName });
+    if (!plan) {
+      const error = new Error("This plan does not exists");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const totalAmount = plan["Pricing"][duration]["Price"].split(" ")[0];
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "INR",
+      amount: totalAmount * 100,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+
+    res.send({ clientSecret: paymentIntent.client_secret });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 });
 
 module.exports = router;
--- frontend/src/pages/Billing.jsx
+++ frontend/src/pages/Billing.jsx
@@ -2,10 +2,12 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { useEffect, useState } from "react";
 import CheckoutForm from "../components/CheckoutForm";
+import { useParams } from "react-router";
 
 const Billing = () => {
   const [stripePromise, setStripePromise] = useState(null);
   const [clientSecret, setClientSecret] = useState("");
+  const { selectedPlan, duration } = useParams();
 
   useEffect(() => {
     async function f1() {
@@ -20,7 +22,10 @@ const Billing = () => {
     async function f2() {
       const response = await fetch("http://localhost:8080/create-payment", {
         method: "POST",
-        body: JSON.stringify({}),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ selectedPlan, duration }),
       });
       const { clientSecret } = await response.json();
       setClientSecret(clientSecret);
